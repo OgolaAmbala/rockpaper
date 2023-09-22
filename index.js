5d3517e10a653f75a653f6deecb9df0b3b617f03
Ogola Ambala <66788503+OgolaAmbala@users.noreply.github.com>
2023-09-22 11:46:00 +0100

    Completed function to run single round to rock, paper, scissors

--- index.js
+++ index.js
@@ -1,7 +1,29 @@
-let guess = ["Rock", "Paper", "Scissors"];
+let guess = ["rock", "paper", "scissors"];
 
 function getComputerChoice() {
   let answer = Math.floor(Math.random() * guess.length);
   return guess[answer];
 }
-console.log(getComputerChoice());
+
+function playRound() {
+  let playerSelection = prompt("Rock, Paper, Scissors").toLowerCase();
+  let computerSelection = getComputerChoice();
+
+  if (playerSelection === computerSelection) {
+    return "It's a tie!";
+  } else if (
+    (playerSelection === "rock" && computerSelection === "scissors") ||
+    (playerSelection === "scissors" && computerSelection === "paper") ||
+    (playerSelection === "paper" && computerSelection === "rock")
+  ) {
+    return "Player wins!";
+  } else {
+    return "Computer wins!";
+  }
+}
+
+console.log(playRound());
+
+//Rock wins against scissors.
+//Scissors win against paper.
+//Paper wins against rock.
